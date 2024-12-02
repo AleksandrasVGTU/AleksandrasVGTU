a7f28827268df5cb21ade0ca563573d0bd3aabb8
AleksandrasVGTU <aleksandras.salaikiskis@stud.vilniustech.lt>
2024-12-02 10:03:49 +0200

    added clock

--- Java.js
+++ Java.js
@@ -1,3 +1,14 @@
 document.getElementById("theme-toggle").addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
 });
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+
+updateClock();
+setInterval(updateClock, 1000);
